72d4336093b6b6a9615473a9d5fb07c7630b25d1
kev560 <singhkevin132@gmail.com>
2015-04-24 16:20:23 -0700

    Edit

---
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+
+{
+	string name;
+	cout << "Enter your name :" << endl;
+	cin >> name;
+	cout << name << " ,Hello World!" << endl;
+	system("pause");
+	return 0;
+
+}
